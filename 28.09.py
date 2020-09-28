7713209c41f81ed125d7b3c27d1d556e60aa9a82
kostya_god <ufhhy123@gmail.com>
2020-09-28 18:19:16 +0300

    First task comleted

--- 28.09.py
+++ 28.09.py
@@ -1,3 +1,4 @@
+import sys
 n= int(input())
 def Fib(n):
     fib1 = fib2 = 1
@@ -9,5 +10,20 @@ def Fib(n):
         n -= 1
 
     return fib2
+if __name__ == "__main__":
+    if len (sys.argv) == 1:
+        print(Fib(n))
+    else:
+        if len(sys.argv) == 2:
+            if sys.argv[1] == '-n':
+                print(Fib(n))
+            else:
+                print('Unvalid argument')
+                sys.exit(1)
+        else:
+            print('Incorrect number of arguments')
+            sys.exit(1)
 
-print(Fib(n))
+
+
+
