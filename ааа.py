7b89dcbaf81f0b63c46fe761d61944fb9a794381
kostya_god <ufhhy123@gmail.com>
2020-09-28 17:51:36 +0300

    First task

--- ааа.py
+++ ааа.py
@@ -1,1 +1,13 @@
-print('Hello ueban')
+n= int(input())
+def Fib(n):
+    fib1 = fib2 = 1
+
+    n = n - 2
+
+    while n > 0:
+        fib1, fib2 = fib2, fib1 + fib2
+        n -= 1
+
+    return fib2
+
+print(Fib(n))
